bd3dc0b38e124c9b47d3900b40d719cd0492131c
AlexField442 <alexfield290@gmail.com>
2025-07-18 20:43:14 +1000

    Remove need to convert priority in Obj1C and Obj71
    As this requires the data to be even-aligned, Obj1C_Radii has also been merged into the table.

--- s2.asm
+++ s2.asm
@@ -23503,99 +23503,59 @@ Obj1C_Index:	offsetTable
 		offsetTableEntry.w Obj1C_Init		; 0
 		offsetTableEntry.w BranchTo_MarkObjGone	; 2
 ; ===========================================================================
-
-objsubdecl macro frame, mapaddr,artaddr,width,priority
-	dc.l frame<<24|mapaddr
-	dc.w artaddr
-	dc.b width, priority
-    endm
-
-; dword_111E6:
-Obj1C_InitData:
-	objsubdecl 0, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,2,0), 4, 6
-	objsubdecl 1, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,2,0), 4, 6
-	objsubdecl 1, Obj11_MapUnc_FC70,  make_art_tile(ArtTile_ArtNem_EHZ_Bridge,2,0), 4, 1
-	objsubdecl 2, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,1,0), $10, 6
-	objsubdecl 3, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), 8, 4
-	objsubdecl 4, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), 8, 4
-	objsubdecl 1, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), $20, 1
-	objsubdecl 0, Obj1C_MapUnc_113D6, make_art_tile(ArtTile_ArtKos_LevelArt,2,0), 8, 1
-	objsubdecl 1, Obj1C_MapUnc_113D6, make_art_tile(ArtTile_ArtKos_LevelArt,2,0), 8, 1
-	objsubdecl 0, Obj1C_MapUnc_113EE, make_art_tile(ArtTile_ArtUnc_Waterfall3,2,0), 4, 4
-	objsubdecl 0, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 1, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 2, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 3, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 4, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 5, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, 4
-	objsubdecl 0, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), $18, 4
-	objsubdecl 1, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), $18, 4
-	objsubdecl 2, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, 4
-	objsubdecl 3, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, 4
-	objsubdecl 4, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, 4
-; byte_1128E:
-Obj1C_Radii:
-	dc.b   0
-	dc.b   0	; 1
-	dc.b   0	; 2
-	dc.b   0	; 3
-    if fixBugs
-	; These are the stakes that the ziplines are attached to in Hill Top Zone.
-	; Using 0 here is good for objects that are at most 32 pixels tall, but these are 40
-	; pixels tall, so they need to be explicitly set here.
-	; This fixes these objects disappearing when they're partially off-screen vertically.
-	dc.b  40	; 4
-	dc.b  40	; 5
-    else
-	dc.b   0	; 4
-	dc.b   0	; 5
-    endif
-	dc.b   0	; 6
-	dc.b   0	; 7
-	dc.b   0	; 8
-	dc.b   0	; 9
-	dc.b   0	; 10
-	dc.b   0	; 11
-	dc.b   0	; 12
-	dc.b $30	; 13
-	dc.b $40	; 14
-	dc.b $60	; 15
-	dc.b   0	; 16
-	dc.b   0	; 17
-	dc.b $30	; 18
-	dc.b $40	; 19
-	dc.b $50	; 20
-	dc.b   0	; 21
-	even
-; ===========================================================================
 ; loc_112A4:
 Obj1C_Init:
 	addq.b	#2,routine(a0)
 	moveq	#0,d0
 	move.b	subtype(a0),d0
-	move.w	d0,d1
-	lsl.w	#3,d0
-	lea	Obj1C_InitData(pc),a1
-	lea	(a1,d0.w),a1
+	mulu.w	#$A,d0
+	lea	Obj1C_InitData(pc,d0.w),a1
 	move.b	(a1),mapping_frame(a0)
 	move.l	(a1)+,mappings(a0)
 	move.w	(a1)+,art_tile(a0)
 	bsr.w	Adjust2PArtPointer
 	ori.b	#4,render_flags(a0)
+	move.w	(a1)+,priority(a0)
 	move.b	(a1)+,width_pixels(a0)
-	move.b	(a1)+,priority(a0)
-	move.w	priority(a0),d0
-	lsr.w	#1,d0
-	andi.w	#$380,d0
-	move.w	d0,priority(a0)
-	lea	Obj1C_Radii(pc),a1
-	move.b	(a1,d1.w),d1
+	move.b	(a1)+,d1
 	beq.s	BranchTo_MarkObjGone	; if the radius is zero, branch
 	move.b	d1,y_radius(a0)
 	bset	#4,render_flags(a0)
 
 BranchTo_MarkObjGone ; BranchTo
 	bra.w	MarkObjGone
+; ===========================================================================
+
+objsubdecl macro frame, mapaddr,artaddr,width,priority,radix
+	dc.l frame<<24|mapaddr
+	dc.w artaddr, priority
+	dc.b width, radix
+    endm
+
+; dword_111E6:
+Obj1C_InitData:
+	objsubdecl 0, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,2,0), 4, $300, 0
+	objsubdecl 1, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,2,0), 4, $300, 0
+	objsubdecl 1, Obj11_MapUnc_FC70,  make_art_tile(ArtTile_ArtNem_EHZ_Bridge,2,0), 4, $80, 0
+	objsubdecl 2, Obj1C_MapUnc_11552, make_art_tile(ArtTile_ArtNem_BoltEnd_Rope,1,0), $10, $300, 0
+	objsubdecl 3, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), 8, $200, 40
+	objsubdecl 4, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), 8, $200, 40
+	objsubdecl 1, Obj16_MapUnc_21F14, make_art_tile(ArtTile_ArtNem_HtzZipline,2,0), $20, $80, 0
+	objsubdecl 0, Obj1C_MapUnc_113D6, make_art_tile(ArtTile_ArtKos_LevelArt,2,0), 8, $80, 0
+	objsubdecl 1, Obj1C_MapUnc_113D6, make_art_tile(ArtTile_ArtKos_LevelArt,2,0), 8, $80, 0
+	objsubdecl 0, Obj1C_MapUnc_113EE, make_art_tile(ArtTile_ArtUnc_Waterfall3,2,0), 4, $200, 0
+	objsubdecl 0, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, 0
+	objsubdecl 1, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, 0
+	objsubdecl 2, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, 0
+	objsubdecl 3, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, $30
+	objsubdecl 4, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, $40
+	objsubdecl 5, Obj1C_MapUnc_11406, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 4, $200, $60
+	objsubdecl 0, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), $18, $200, 0
+	objsubdecl 1, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), $18, $200, 0
+	objsubdecl 2, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, $200, $30
+	objsubdecl 3, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, $200, $40
+	objsubdecl 4, Obj1C_MapUnc_114AE, make_art_tile(ArtTile_ArtNem_Oilfall2,2,0), 8, $200, $50
+
 ; ===========================================================================
 ; ----------------------------------------------------------------------------
 ; Object 71 - Bridge stake and pulsing orb from Hidden Palace Zone
@@ -23614,29 +23574,25 @@ Obj71_Index:	offsetTable
 ; ---------------------------------------------------------------------------
 ; dword_11302:
 Obj71_InitData:
-	objsubdecl 3, Obj11_MapUnc_FC28,  make_art_tile(ArtTile_ArtNem_HPZ_Bridge,3,0), 4, 1		; Hidden Palace bridge
-	objsubdecl 0, Obj71_MapUnc_11396, make_art_tile(ArtTile_ArtNem_HPZOrb,3,1), $10, 1		; Hidden Palace pulsing orb
-	objsubdecl 0, Obj71_MapUnc_11576, make_art_tile(ArtTile_ArtNem_MtzLavaBubble,2,0), $10, 1	; MTZ lava bubble
+	objsubdecl 3, Obj11_MapUnc_FC28,  make_art_tile(ArtTile_ArtNem_HPZ_Bridge,3,0), 4, $80, 0	; Hidden Palace bridge
+	objsubdecl 0, Obj71_MapUnc_11396, make_art_tile(ArtTile_ArtNem_HPZOrb,3,1), $10, $80, 0		; Hidden Palace pulsing orb
+	objsubdecl 0, Obj71_MapUnc_11576, make_art_tile(ArtTile_ArtNem_MtzLavaBubble,2,0), $10, $80, 0	; MTZ lava bubble
 ; ===========================================================================
 ; loc_1131A:
 Obj71_Init:
 	addq.b	#2,routine(a0)
+	moveq	#0,d0
 	move.b	subtype(a0),d0
 	andi.w	#$F,d0
-	lsl.w	#3,d0
-	lea	Obj71_InitData(pc),a1
-	lea	(a1,d0.w),a1
+	mulu.w	#$A,d0
+	lea	Obj71_InitData(pc,d0.w),a1
 	move.b	(a1),mapping_frame(a0)
 	move.l	(a1)+,mappings(a0)
 	move.w	(a1)+,art_tile(a0)
 	bsr.w	Adjust2PArtPointer
 	ori.b	#4,render_flags(a0)
+	move.w	(a1)+,priority(a0)
 	move.b	(a1)+,width_pixels(a0)
-	move.b	(a1)+,priority(a0)
-	move.w	priority(a0),d0
-	lsr.w	#1,d0
-	andi.w	#$380,d0
-	move.w	d0,priority(a0)
 	move.b	subtype(a0),d0
 	andi.w	#$F0,d0
 	lsr.b	#4,d0
